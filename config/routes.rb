28daea5a987508adae27816d142502a93f2a4f7b
shotaimai66 <changemymind6@gmail.com>
2022-03-01 08:03:38 +0900

    devise導入完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+  devise_for :managers, controllers: {
+    sessions:      'managers/sessions',
+    passwords:     'managers/passwords',
+    registrations: 'managers/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  # root to: 'home#index'
 end
