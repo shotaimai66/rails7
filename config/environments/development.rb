563ad124bd4c1442c53a64f03777333c37ac2f50
shotaimai66 <changemymind6@gmail.com>
2022-03-24 05:36:28 +0900

    web-consoleの設定追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.web_console.whitelisted_ips = %w[127.0.0.1 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16]
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
