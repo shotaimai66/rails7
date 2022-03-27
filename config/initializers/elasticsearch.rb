b1b32d997132bbf98d78e9e6ee69b6427902bb00
shotaimai66 <changemymind6@gmail.com>
2022-03-27 19:30:40 +0900

    fix

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,10 +1,16 @@
-Elasticsearch::Model.client =
-  Elasticsearch::Client.new(
-    hosts: [
-      {
-        host: 'elasticsearch',
-        port: '9200'
-      }
-    ],
-    url: ENV.fetch("BONSAI_URL") { 'http://localhost' }
-  )
+if Rails.env.production?
+  Elasticsearch::Model.client =
+    Elasticsearch::Client.new(
+      url: ENV['BONSAI_URL']
+    )
+else
+  Elasticsearch::Model.client =
+    Elasticsearch::Client.new(
+      hosts: [
+        {
+          host: 'elasticsearch',
+          port: '9200'
+        }
+      ]
+    )
+end
