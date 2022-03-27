3661c1f94e4614e2fbf8356397d3d931b5758c4b
shotaimai66 <changemymind6@gmail.com>
2022-03-27 19:19:34 +0900

    fix heroku.yaml

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,7 +1,10 @@
 Elasticsearch::Model.client =
-  Elasticsearch::Client.new hosts: [
-    {
-      host: 'elasticsearch',
-      port: '9200'
-    }
-]
+  Elasticsearch::Client.new(
+    hosts: [
+      {
+        host: 'elasticsearch',
+        port: '9200'
+      }
+    ],
+    url: ENV.fetch("BONSAI_URL") { 'http://localhost' }
+  )
